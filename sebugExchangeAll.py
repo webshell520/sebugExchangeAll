c68916d64615f054fb252a28b4fa00a602bac264
Your Name <neargle@outlook.com>
2016-04-09 01:33:40 -0700

    sebug cookie program

--- sebugExchangeAll.py
+++ sebugExchangeAll.py
@@ -33,7 +33,7 @@ headers = {
     'Origin': 'https://www.seebug.org',
     'X-Requested-With': 'XMLHttpRequest',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36',
-    'X-CSRFToken': '4AGR8HtjYGKX8kXkdaf0BQsQ3159eqMg',
+    'X-CSRFToken': os.environ.get('csrftoken'),
     'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
     'Referer': 'https://www.seebug.org/',
     'Accept-Encoding': 'gzip, deflate',
@@ -41,23 +41,32 @@ headers = {
 }
 
 
-params = {
-    'sessionKey': os.environ.get('sessionKey'),
-    'csrftoken': os.environ.get('csrftoken')
-}
+sessionKey = {'sessionKey': os.environ.get('sessionKey')}
+csrftoken = {'csrftoken': os.environ.get('csrftoken')}
+
+seebug_s = requests.session()
+seebug_s.headers.update(headers)
+seebug_s.cookies.update(csrftoken)
+seebug_s.cookies.update(sessionKey)
+# import pdb; pdb.set_trace()
+
 
 def exchange(ssvid=0):
-    url = 'https://www.seebug.org/vuldb/ssvid-' + str(ssvid)
-    poc_r = requests.post(
-        url, params=params, headers=headers,
+    # print cookies
+    url = 'https://www.seebug.org/vuldb/exchange/' + str(ssvid)
+    poc_r = seebug_s.post(
+        url,
         data=poc_payload, verify=False
     )
-    detail_r = requests.post(
-        url, params=params, headers=headers,
+    detail_r = seebug_s.post(
+        url,
         data=detail_payload, verify=False
     )
+    print seebug_s.cookies
     print poc_r.content
-    print detail_r.content
+    print poc_r.cookies
+    print poc_r.content
+    print poc_r.cookies
 
 
 if __name__ == '__main__':
